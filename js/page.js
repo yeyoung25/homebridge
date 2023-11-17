833723345600c406671212b1c94d9b29f7900b62
yeyoung25 <sevim0105@gmail.com>
2023-11-18 03:24:26 +0900

    Update HOMEBRIDGE.html 231118 03

--- js/page.js
+++ js/page.js
@@ -1,3 +1,33 @@
-$(function(){
-    
+$(document).ready(function(){
+    navToggle($("div div:first-child nav div"));
+    newspage();
+    trafTab();
+    backBtn($(".backBtn"));
 });
+
+function navToggle(button){
+    $(button).click(function(){
+        $(this).toggleClass("active");
+    });
+}
+
+function newspage(){
+    $(".lastP").click(function(e){
+        alert("This is the last page.");
+    });
+  };
+
+  function trafTab(){
+    $('.trafCBContainer div:nth-child(2) ul li').click(function(){
+      $('.trafCBContainer div:nth-child(2) ul li').removeClass('active');
+      $(this).addClass('active');
+      $('.trafCBContainer div:nth-child(2) div').removeClass('active');
+      $('.' + $(this).attr('data-tab')).addClass('active');
+    });
+  }
+
+  function backBtn(button){
+    $(button).click(function(){
+      history.go(-1);
+    });
+  }
